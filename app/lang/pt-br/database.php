882d5d727860b25c0a852f9f7dfedd5ddacaa88d
Vitor Pliavi Silvério de Souza <pliavi.rpgm@gmail.com>
2016-12-29 17:52:55 -0200

    adicionado arquivo de tradução do banco para os erros de validação

---
+++ app/lang/pt-br/database.php
@@ -0,0 +1,34 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Campos de Banco de Dados
+	|--------------------------------------------------------------------------
+	|
+	| Aqui são traduzidos os nomes dos campos do banco de
+  | dados para utilização geral
+	|
+	*/
+
+  'Manifestations'   => 'Manifestação',
+  'Claimants'        => 'Contribuinte',
+  'Individuals'      => 'Pessoa Física',
+  'Companies'        => 'Pessoa Jurídica',
+  'Types'            => 'Tipo de Resposta',
+  'Identifications'  => 'Identificação do Contribuinte',
+  'Attachments'      => 'Anexo(s)',
+  'FactAddress'      => 'Endereço do Fato',
+  'Answers'          => 'Opção de Resposta',
+  'Addresses'        => 'Endereço',
+  'Telephones'       => 'Telefone',
+  'Emails'           => 'Email',
+  'Users'            => 'Usuário',
+  'Profiles'         => 'Perfil',
+  'Units'            => 'Unidade',
+  'Sectors'          => 'Setor',
+  'Subjects'         => 'Assunto',
+  'SubjectDeadLines' => 'Prazo',
+  'Notifications'    => 'Notificação',
+);
