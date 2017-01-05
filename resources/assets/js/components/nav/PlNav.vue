1ba655a0434f0a58285f12c03a3a1e66f6e74610
Pliavi <Pliavi.rpgm@gmail.com>
2017-01-05 00:37:46 -0200

    added input and nav vue components

---
+++ resources/assets/js/components/nav/PlNav.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+  <nav class="nav has-shadow is-primary">
+    <div class="nav-left">
+
+      <pl-tab class="is-brand is-active" link="home">
+        <strong>PL-OMB</strong>
+      </pl-tab>
+
+      <pl-tab link="manifestation.create">
+        Abrir Manifestação
+      </pl-tab>
+
+      <pl-tab link="manifestation.search">
+        Consultar Manifestação
+      </pl-tab>
+
+      <pl-tab link="admin.reports" v-if="user && user.reports">
+        Relatórios
+      </pl-tab>
+
+      <div class="nav-right nav-menu" v-if="user">{{ user.name }}</div>
+
+    </div>
+  </nav>
+</template>
+
+<script>
+  import PlTab from './PlTab.vue'
+
+  export default{
+    data() {
+      return {
+        user: {}
+      }
+    },
+
+    mounted() {
+      this.axios.get("loggedUser").then((response) => {
+        this.user = response.data;
+      })
+    },
+
+    components: { PlTab },
+  }
+</script>
